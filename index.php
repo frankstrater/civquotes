6059fd1664a56b6c2da902c48ec0dfbbc9850e3e
frankstrater <frankstrater@gmail.com>
2021-08-04 13:58:45 +0200

    Add Nimoy bot and update old web version and source data

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 	/*
 	$quotes_data = array();
 
-	$xml_techs = simplexml_load_file('Civ4GameTextInfos_Objects.xml');
-	$xml_quotes = simplexml_load_file('Civ4GameTextInfos.xml');
+	$xml_techs = simplexml_load_file('CIV4GameTextInfos_Objects.xml');
+	$xml_quotes = simplexml_load_file('CIV4GameTextInfos.xml');
 
 	$i = 0;
 
 	foreach ($xml_techs as $tech):
 		if (substr($tech->Tag, 0, 13) == "TXT_KEY_TECH_") {
-			$str_audio = (string) str_replace("TXT_KEY_TECH_", "", $tech->Tag);
+			$str_audio = (string) strtolower(str_replace("TXT_KEY_TECH_", "", $tech->Tag));
 			$quotes_data[$i]['title'] = (string) $tech->English;
-			$quotes_data[$i]['audio'] = 'Tech_'.str_replace("_", "", $str_audio).'.mp3';
+			$quotes_data[$i]['audio'] = 'Tech_'.str_replace(' ', '', ucwords(str_replace('_', ' ', $str_audio))).'.mp3';
 			$i++;
 		}
 	endforeach;
@@ -57,12 +57,7 @@
 
 	$title = $quotes[$q]['title'];
 	$quote_full = $quotes[$q]['description'];
-
-	if ($title == 'Code of Laws') {
-		$title = 'Code Of Laws';
-	}
-
-	$audio = 'Tech_'.str_replace(' ', '', $title).'.mp3';
+	$audio = $quotes[$q]['audio'];
 
 	$blockquote = explode('" - ', $quote_full);
 	$quote = '&#8220;'.substr($blockquote[0], 1).'&#8221';
@@ -70,21 +65,18 @@
 ?>
 <!DOCTYPE html>
 <html lang="en" prefix="og: http://ogp.me/ns#">
-  <head>
-    <meta charset="utf-8"/>
-    <title>CivQuotes: <?= $title ?></title>
-    <meta name="description" content="<?= $quote.$cite ?>">
-    <meta property="og:title" content="<?= $title ?>">
+	<head>
+	<meta charset="utf-8"/>
+	<title>CivQuotes: <?= $title ?></title>
+	<meta name="description" content="<?= $quote.$cite ?>">
+	<meta property="og:title" content="<?= $title ?>">
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="<?= $base_url ?>/index/<?= $q ?>">
+	<meta property="og:url" content="<?= $base_url ?>/index.php/<?= $q ?>">
 	<meta property="og:image" content="<?= $base_url ?>/assets/civ4icon.png" />
 	<meta property="og:site_name" content="CivQuotes">
 	<meta property="og:description" content="<?= $quote.$cite ?>">
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-    <link rel="stylesheet" media="all" href="<?= $base_url ?>/assets/screen.css"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+	<link rel="stylesheet" media="all" href="<?= $base_url ?>/assets/screen.css"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
 </head>
 <body>
 	<h4><?= $title ?></h4>
@@ -99,14 +91,10 @@
 		[Your browser does not support the audio element.]
 	</audio>
 	<br>
-	<a href="https://twitter.com/share?url=/" class="twitter-share-button" data-lang="en" data-text="<?= $quote.$cite ?>">Tweet</a>
-	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-	<br>
-	<br>
 	<div class="links">
 	<?php
 	foreach ($quotes as $key=>$item) {
-		echo '<a href="'.$base_url.'/index/'.$key.'">';
+		echo '<a href="'.$base_url.'/index.php/'.$key.'">';
 		echo $item['title'];
 		echo '</a><br>';
 	}
@@ -114,31 +102,5 @@
 	</div>
 	<br>
 	<small>Audio files Copyright by Firaxis Games</small>
-	<?php
-
-		if (!(isset($_SERVER['HTTP_DNT']) && $_SERVER['HTTP_DNT'] == 1)) {
-
-	?>
-
-	<script type="text/javascript">
-
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-47310731-1']);
-	  _gaq.push(['_setDomainName', 'strateradvies.nl']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-
-	</script>
-
-	<?php
-
-		}
-
-	?>
 </body>
 </html>
---
+++ nimoy_bot.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import requests
+import json
+
+# Config
+
+api_url = 'https://botsin.space/api/v1'
+account_id = '106691813787549443'
+access_token = 'xxxxxxxxxx'
+
+# Authorization header
+
+headers = {
+	'Authorization': 'Bearer ' + access_token
+}
+
+# Get statuses count
+
+response = requests.get(api_url + '/accounts/' + account_id)
+data = response.json()
+
+statuses_count = data['statuses_count']
+
+# Read quotes file
+
+with open('assets/civquotes.json', 'r') as quotes_file:
+	data = quotes_file.read()
+
+# Parse quotes file
+
+quotes = json.loads(data)
+
+# Get next quote
+
+quote_key = list(quotes.keys())[statuses_count]
+quote_values = list(quotes.values())[statuses_count]
+
+tag = '#' + quote_key
+file = quote_values['audio']
+description = quote_values['description']
+
+description_split = description.split(' - ')
+cite = description_split.pop(-1)
+quote = (' - ').join(description_split)
+
+status = tag + '\n\n' + quote + '\n\n' + '- ' + cite
+
+# Upload media
+
+files = {
+	'file': (file, open('assets/' + file, 'rb'))
+}
+
+response = requests.post(api_url + '/media', headers=headers, files=files)
+data = response.json()
+
+media_ids = [data['id']]
+
+# Post status
+
+headers['Content-Type'] = 'application/json'
+
+data = {
+	'status': status,
+	'media_ids': media_ids
+}
+
+requests.post(api_url + '/statuses', headers=headers, data=json.dumps(data))
